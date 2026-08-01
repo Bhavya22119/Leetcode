3f29e46003e00b2d1a72d42a814eaeab1ba90d5f
Bhavya22119 <bhavyachhimpa0001@gmail.com>
2026-08-01 23:43:44 +0530

    solved 486

---
+++ Problems/486. Predict the Winner.js
@@ -0,0 +1,26 @@
+// Senior Staff
+// Array
+// Math
+// Dynamic Programming
+// Recursion
+// Game Theory
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var predictTheWinner = function(nums) {
+  const n = nums.length;
+  const dp = [...nums]; 
+
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = i + 1; j < n; j++) {
+      dp[j] = Math.max(
+        nums[i] - dp[j],    
+        nums[j] - dp[j - 1]  
+      );
+    }
+  }
+
+  return dp[n - 1] >= 0;
+};
